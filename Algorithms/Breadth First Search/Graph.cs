5207c19e1c7a800acbb6c4622b9e9ce1af8d910d
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2023-12-12 11:33:29 -0300

    Breadth First Search

---
+++ Algorithms/Breadth First Search/Graph.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private int V; // Número de vértices
+    private List<int>[] adj; // Lista de adjacências
+
+    public Graph(int v)
+    {
+        V = v;
+        adj = new List<int>[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new List<int>();
+    }
+
+    public void AddEdge(int v, int w)
+    {
+        adj[v].Add(w);
+    }
+
+    public List<int> BFS(int s, int target)
+    {
+        bool[] visited = new bool[V];
+        int[] parent = new int[V];
+        for (int i = 0; i < V; ++i)
+        {
+            visited[i] = false;
+            parent[i] = -1;
+        }
+
+        Queue<int> queue = new Queue<int>();
+        visited[s] = true;
+        queue.Enqueue(s);
+
+        while (queue.Count != 0)
+        {
+            int u = queue.Dequeue();
+
+            foreach (int v in adj[u])
+            {
+                if (!visited[v])
+                {
+                    visited[v] = true;
+                    queue.Enqueue(v);
+                    parent[v] = u;
+
+                    if (v == target)
+                        return GetPath(parent, s, v);
+                }
+            }
+        }
+
+        return null; // Se o alvo não for alcançado
+    }
+
+    private List<int> GetPath(int[] parent, int s, int v)
+    {
+        List<int> path = new List<int>();
+        for (int i = v; i != s; i = parent[i])
+            path.Add(i);
+        path.Add(s);
+        path.Reverse();
+        return path;
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,9 +7,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] list = { 7, 3, 25, 9, 5, 2, 8, 4, 6 };
+        Graph g = new Graph(4);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(1, 2);
+        g.AddEdge(2, 0);
+        g.AddEdge(2, 3);
+        g.AddEdge(3, 3);
 
-        Console.WriteLine("QuickSort List: ");
-        SelectionSort.printList(list);
+        int source = 0;
+        int target = 3;
+
+        List<int> path = g.BFS(source, target);
+
+        if (path != null)
+        {
+            Console.Write($"Caminho de {source} para {target}: ");
+            foreach (int vertex in path)
+                Console.Write($"{vertex} ");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine($"Não há caminho de {source} para {target}");
+        }
     }
 }
