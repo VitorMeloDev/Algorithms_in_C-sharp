034d56365dd2cb8f732d119fa0a694e576c4c6f9
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2023-12-21 01:07:03 -0300

    Learn Graphs Algorithms

--- Algorithms/Breadth First Search/Graph.cs
+++ Algorithms/Breadth First Search/Graph.cs
@@ -1,39 +1,92 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 class Graph
 {
+    // DFS with recursion
     public static void DepthFirstSearch(Dictionary<char, List<char>> graph, char source)
+    {
+        Console.WriteLine(source);
+
+        foreach (char neighbor in graph[source])
+        {
+            DepthFirstSearch(graph, neighbor);
+        }
+    }
+
+    // DFS with Stack
+    public static void DepthFirstSearch2(Dictionary<char, List<char>> graph, char source)
     {
         Stack<char> stack = new Stack<char>();
         stack.Push(source);
 
-        while (stack.Count > 0)
+        while(stack.Count > 0)
         {
             char current = stack.Pop();
             Console.WriteLine(current);
 
-            foreach (char neighbor in graph[current])
+            foreach(char neighbor in graph[current])
             {
                 stack.Push(neighbor);
             }
         }
     }
 
+    // BFS
     public static void BreadthFirstSearch(Dictionary<char, List<char>> graph, char source)
     {
         Queue<char> queue = new Queue<char>();
         queue.Enqueue(source);
 
-        while (queue.Count > 0)
+        while(queue.Count > 0)
         {
             char current = queue.Dequeue();
             Console.WriteLine(current);
 
-            foreach (char neighbor in graph[current])
+            foreach(char neighbor in graph[current])
             {
                 queue.Enqueue(neighbor);
             }
         }
+    }
+
+
+    // Has Path with recursion
+    public static void HasPath(Dictionary<char, List<char>> graph, char source, char destiny)
+    {
+        if(source == destiny)
+        {
+            Console.WriteLine("Has Path!");
+            return;
+        }
+        else
+        {
+            foreach(char neighbor in graph[source])
+            {
+                HasPath(graph, neighbor, destiny);
+            }
+        }
+    }
+
+    public static void HasPath2(Dictionary<char, List<char>> graph, char source, char destiny)
+    {
+        Queue<char> queue = new Queue<char>();
+        queue.Enqueue(source);
+
+        while(queue.Count > 0)
+        {
+            char current = queue.Dequeue();
+
+            if (current == destiny) { Console.WriteLine("Has Path!"); return; }
+
+            foreach(char neighbor in graph[current])
+            {
+                queue.Enqueue(neighbor);
+            }
+        }
+
+        Console.WriteLine("Hasn't path!");
+        return;
     }
 }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,19 +7,22 @@ class Program
 {
     static void Main(string[] args)
     {
-       Dictionary<char,List<char>>  graph = new Dictionary<char, List<char>>();
+        Dictionary<char, List<char>> graph = new Dictionary<char, List<char>>();
 
-        graph['A'] = new List<char> { 'C', 'B' };
-        graph['B'] = new List<char> { 'D'};
-        graph['C'] = new List<char> { 'E' };
-        graph['D'] = new List<char> { 'F'};
+        graph['A'] = new List<char> {'B', 'C' };
+        graph['B'] = new List<char> {'D' };
+        graph['C'] = new List<char> {'E' };
+        graph['D'] = new List<char> {'F' };
         graph['E'] = new List<char> {};
         graph['F'] = new List<char> {};
 
-        Console.WriteLine("DFS: ");
-        Graph.DepthFirstSearch(graph, 'A'); // ABDFCE
+        Console.WriteLine("Depth First Search: ");
+        Graph.DepthFirstSearch(graph, 'A');
 
-        Console.WriteLine("BFS: ");
-        Graph.BreadthFirstSearch(graph, 'A'); // ACBEDF
+        Console.WriteLine("Breadth First Search: ");
+        Graph.BreadthFirstSearch(graph, 'A');
+
+        Console.WriteLine("Has Path between B and F? ");
+        Graph.HasPath2(graph, 'B', 'F');
     }
 }
